ea9f27447a843aa98fda5c49733d212a5461fe86
Thomas Arreola <thomasarreolaprofessional@gmail.com>
2026-05-06 13:54:14 -0700

    finished expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,7 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	return JSON.parse(localStorage.getItem('recipes')) || 0; 
 }
 
 /**
@@ -35,10 +36,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const mainElement = document.querySelector("main");
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	for(let recipe in recipes){
+		const recipeCard = document.createElement("recipe-card");
+		recipeCard.data = recipe;
+		mainElement.append(recipeCard);
+	}
 }
 
 /**
